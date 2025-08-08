8cabe52c83ee0ccb7b26dd0e03ab8d3f00c1abe1
Mahafujmr <mrtuhin444@gmail.com>
2025-08-08 17:19:58 +0530

    dart Data type

---
+++ bin/data_type/dart_data_type.dart
@@ -0,0 +1,27 @@
+// dart data type
+/*
+----> Dart Data Type ---->
+Integer -> 1,10,222,34556 --- > int
+Floating -> 2.33,  3.3 , 23.33 --> double
+string -- > A, "Any Name","Address" -->string
+Boolean --> true, or false --> bool
+-----------------------
+---- Variable creation syntax--->
+Container put -> value
+data type name -> variable name -->value
+ */
+void main(){
+  // integer type
+  int number = 100;
+  // floating type - double
+  double costNumber = 3.445;
+  // string type
+  String accountName = "Tuhin";
+  // bool type
+  bool accountAddress = true;
+  print(number);
+  print(costNumber);
+  print(accountName);
+  print(accountAddress);
+
+}
