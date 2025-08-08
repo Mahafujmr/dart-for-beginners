6c52962ee81f202aebf7815c7a47a40ec614df28
Mahafujmr <mrtuhin444@gmail.com>
2025-08-09 00:08:52 +0530

    Dart All Data type

---
+++ bin/data_type/dart_all_data_type/double_data_type.dart
@@ -0,0 +1,8 @@
+// double data type
+void main(){
+  // double data type ->
+  double totalNumber = 3.44;
+  double totalAmount = 222.33;
+  print(totalNumber);
+  print(totalAmount);
+}
---
+++ bin/data_type/dart_all_data_type/integer_data_type.dart
@@ -0,0 +1,7 @@
+// dart integer data type
+void main(){
+  int rollNumber = 10; // int type
+  int classRoll = 22;
+  print(rollNumber);
+  print(classRoll);
+}
