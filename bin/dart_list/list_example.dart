3957d738ca055c9042fb312a0f56f724eb864b6e
Mahafujmr <mrtuhin444@gmail.com>
2025-08-09 02:02:13 +0530

    dart List

---
+++ bin/dart_list/list_example.dart
@@ -0,0 +1,21 @@
+// write a programme dart list
+/*
+-- < > --> Angle Brackets
+[] --> Square Brackets
+--> creation List-
+--> List < Data type name> variableName = [assign value] ; -> semicolon
+-------- List of Data type --------
+1->List <String > teacherName = [add teacher name ];--> This is String type list
+2->List <int> number = [add integer number];--> This is int type list.
+3->List<double> cgpNumber = [add double number];->This is double type list.
+4->List<bool> countryName = [add bool ];-> This is boolean type list.
+ */
+
+void main(){
+  List <String> studentName = ['Tuhin','Toma','Mahafuja','My Father','Karim'];
+  print(studentName);
+  print(studentName.first);
+  print(studentName.last);
+  print(studentName.length);
+
+}
