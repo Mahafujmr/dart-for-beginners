24f5974f6d05e30381ff3d2335d4d498daebc9ac
Mahafujmr <mrtuhin444@gmail.com>
2025-08-08 17:25:10 +0530

    integer type problem solve

---
+++ bin/problem_solve/integer_type/problem_one.dart
@@ -0,0 +1,7 @@
+// write a problem integer two number addition
+void main(){
+  int firstNumber=100;
+  int secondNumber = 200;
+  int totalNumber = firstNumber+secondNumber;
+  print(totalNumber);
+}
