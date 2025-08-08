458333a9a66f146053a629ff2a903d07744a7e64
Mahafujmr <mrtuhin444@gmail.com>
2025-08-09 02:33:03 +0530

    dart list example

--- bin/dart_list/list_example.dart
+++ bin/dart_list/list_example.dart
@@ -10,6 +10,15 @@
 3->List<double> cgpNumber = [add double number];->This is double type list.
 4->List<bool> countryName = [add bool ];-> This is boolean type list.
  */
+// ---> List All Properties --->
+/*
+first -> Returns the first element in the list.
+last -> Returns the last element in the list.
+length-> Returns the total element number in the list.
+reversed -> show list elements from last to first.
+empty-> true if the list has no elements
+isNotEmpty -> true if the list one or more element
+ */
 
 void main(){
   List <String> studentName = ['Tuhin','Toma','Mahafuja','My Father','Karim'];
@@ -17,5 +26,7 @@ void main(){
   print(studentName.first);
   print(studentName.last);
   print(studentName.length);
+  print(studentName.reversed);
+  print(studentName.isEmpty);
 
 }
