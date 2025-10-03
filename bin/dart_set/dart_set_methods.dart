c4d52d39ba1472621e599da2bc1487a1f8404ab7
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-03 11:06:25 +0600

    dart set methods code update

--- bin/dart_set/dart_set_methods.dart
+++ bin/dart_set/dart_set_methods.dart
@@ -16,13 +16,15 @@ void main() {
   print('Remove [1, 2, 3, 4] Elements = $houseNumbers');
   // remove where 
   houseNumbers.removeWhere((num) => num %2 == 0);
-  print('Even number remove [2,4,6] = $houseNumbers\n');
+  print('Even number remove [2,4,6] = $houseNumbers');
 
   // value include -> check this value-> true or false
   print(houseNumbers.contains(1));
+
   // clear set
   houseNumbers.clear();
   print('Set is clear = $houseNumbers');
+  print('--Complete Dart Set Methods--');
   
 
 }
