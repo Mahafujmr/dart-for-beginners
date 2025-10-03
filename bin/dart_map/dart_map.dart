3fbcd85d66bb2063a4e33978685d595c8713801a
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-03 23:18:38 +0600

    dart all type map

--- bin/dart_map/dart_map.dart
+++ bin/dart_map/dart_map.dart
@@ -10,22 +10,38 @@ Map<data type name, data type name> variable name = { key : value};
 Map <int , string> studentName = {1:'Tuhin' , 2:'Toma'};
  */
 void main() {
-  // creation map
-  // integer and double type map
+  // Map Creating
+  // integer and double type maps
   Map<int, double> studentMarks = {
     1: 100.10,
     2: 80.30,
     3: 90.33,
     4: 70.55,
   };
-  print('Integer and Double Type Maps = $studentMarks');
-  // integer and string type map
+  print('Integer and Double Type Map = $studentMarks');
+  // integer and string type maps
   Map<int, String> productName = {
     1: 'Computer',
     2: 'Mobile',
     3: 'Tab',
-    4: 'I-Phone',
+    4: 'iPhone',
   };
   print('Int and String Type Map = $productName');
+
+  // string and bool type maps
+  Map<String, bool> userPermission = {
+    'isCode': true,
+    'isPermission': false,
+    'isVerification': true,
+    'isLogin': true,
+  };
+  print('String and Boolean Type Map = $userPermission');
+  // dynamic type maps
+  Map<dynamic, dynamic> dynamicMaps = {
+    1: 'Tamim',
+    'tusherMobile': true,
+    'Result': 3.44,
+  };
+  print('Dynamic Type Map = $dynamicMaps');
 
 }
