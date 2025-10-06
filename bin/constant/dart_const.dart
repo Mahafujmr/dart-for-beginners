a019c23befe1c6837b8f7b15c6fa476e91cbb438
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-06 21:50:14 +0600

    dart constant example

---
+++ bin/constant/dart_const.dart
@@ -0,0 +1,12 @@
+// dart const
+/*
+--> No change value once there are set value
+--> const=> compile time constant
+--> value must be know before run programme
+ */
+void main() {
+String name = 'Tuhin';
+print(name);
+name = 'Toma';
+name = 'MR Tuhin';
+}
