be6fea7f0a1f35c75eed26d298bca91a8c52af38
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-07 00:51:26 +0600

    dart growable list example

---
+++ bin/practice_code/list/grawable_list.dart
@@ -0,0 +1,8 @@
+// dart growable list
+// Growable list add remove or clear
+void main() {
+  var myNumber =[11,22,33,44];
+  print('This is Growable List = $myNumber');
+  myNumber.add(55);
+  print(myNumber);
+}
