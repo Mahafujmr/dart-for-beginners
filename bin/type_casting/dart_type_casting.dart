a2acc7b3d5aefb68cb5458c235a4b7926913e1a1
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-07 17:13:57 +0600

    dart type casting

--- bin/type_casting/dart_type_casting.dart
+++ bin/type_casting/dart_type_casting.dart
@@ -1,16 +1,24 @@
 // dart type casting or type convert
 void main() {
   int roll = 20;
-  double result = 3.50;
+  double result = 3.5555;
   print('My Roll Number is $roll, and My Result is $result');
 
   /*
    type casting int = double
    result is double but casting in int
-   result.toInt()- convert intger
+   result.toInt()- convert integer
    */
   int rollResult = roll + result.toInt();
   print(rollResult);
-  double addResult = roll + result;
+  /*
+  int type casting to double
+  use intValue.to double
+   */
+  double addResult = roll.toDouble() + result;
   print(addResult);
+  // just show 2 or 1 number i mean fixed number show
+  // syntax = doubleVariable.toStringAsFixed
+  print(addResult.toStringAsFixed(2));
+
 }
