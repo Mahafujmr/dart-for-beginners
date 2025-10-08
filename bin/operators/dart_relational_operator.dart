4c9be385e0416eec77d95ad10ea2b6bf41efefaa
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-08 23:02:27 +0600

    dart Relational Operator

---
+++ bin/operators/dart_relational_operator.dart
@@ -0,0 +1,31 @@
+// dart relational operator
+/*
+1) Greater than > | a > b | 10 > 5; is true
+2) Greater than or equal >= | a >= b | 10 >=5 ; true
+3) Less than  < | a < b | 10 < 5; is false
+4) less than or equal <= | a <= b | 10 <= 5 ; false
+5) is equal to == | a == b | 10 == 5 ; false
+6) not equal to != | a != b | 10 != 5; false
+ */
+void main() {
+  int a = 10;
+  int b = 5;
+  print('===> Relational Operator ===>');
+  // a > b = 10 > 5 , this condition is true
+  print('Greater Than (a>b) = ${a > b}');
+
+  // a >= b = 10 >= 5, this condition is true
+  print('Greater Than or equal (a >= b) =  ${a >= b}');
+
+  // a < b = 10 < 5, this condition is false
+  print('Less Than (a < b)  = ${a < b}');
+
+  // a <= b = 10 <= 5 , this condition is false
+  print('Less than or equal (a<=b) = ${a<=b}');
+
+  // a == b = 10 == 5, this condition is false
+  print('Is equal to (a == b) = ${a == b}');
+
+  // a != b = 10 != 5 , this condition is true
+  print('Not equal to (a != b) = ${a != b}');
+}
