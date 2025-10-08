7b2685738c32c0c4506891497d1987bd6e880b96
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-08 12:15:25 +0600

    dart user input

--- bin/practice_code/user_input/user_input.dart
+++ bin/practice_code/user_input/user_input.dart
@@ -5,5 +5,8 @@ void main() {
  // use null(?) operator / Either value or Either null
  String? name = stdin.readLineSync();
  print('My Name is = $name ');
+ print('your Roll Number :');
+ int rollNumber = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
+ print('My Roll Number is = $rollNumber');
 
 }
