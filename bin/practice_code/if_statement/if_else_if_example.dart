f3b0daa06629e9a678e53b159e3db74a2625cdbe
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-12 22:07:59 +0600

    dart if else if example

---
+++ bin/practice_code/if_statement/if_else_if_example.dart
@@ -0,0 +1,13 @@
+// dart if else if example
+void main() {
+  int taka = 100;
+  if(taka >= 200){
+    print('I eat Fuske');
+  }else if(taka >= 120){
+    print('I eat barger');
+  }else if(taka <= 150){
+    print('I eat pizza');
+  }else{
+    print('I have no money');
+  }
+}
