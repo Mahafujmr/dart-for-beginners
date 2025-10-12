7bf6855ef9c8bed5d1533da93bd7fb89f9ce8780
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-12 22:52:49 +0600

    dart if example

---
+++ bin/practice_code/if_statement/if_else_result_marks.dart
@@ -0,0 +1,17 @@
+// if else if result
+main() {
+  int result = 28;
+  if(result >= 80){
+    print('A+');
+  }else if(result >= 70){
+    print('A');
+  }else if(result >= 60){
+    print('A-');
+  }else if(result >= 50){
+    print('B');
+  }else if(result >= 40){
+    print('C');
+  }else{
+    print('Fail');
+  }
+}
