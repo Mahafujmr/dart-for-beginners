b331e607a953eb0f15dd754ae9134f36f5fb9835
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-13 15:47:49 +0600

    dart for loop example

---
+++ bin/dart_loop/dart_for_loop.dart
@@ -0,0 +1,22 @@
+// dart for loop
+/*
+==> how to work loop ==>
+1- o-start
+2- check condition
+3- number increment/ decrement
+4- statement
+==> for loop syntax <==
+start/initialization ; condition ; increment/ decrement
+==> Example
+for(start/initialization ; condition ; increment/ decrement){
+print();
+}
+ */
+void main() {
+for(int a = 0; a <= 100; a++){
+  // use a++ or use a= a+1
+
+  print('This is me $a');
+}
+print('End For Loop');
+}
