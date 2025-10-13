426f58fc7bcba1224fe2be8e9299b5ae0b16aeaa
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-13 19:25:05 +0600

    dart for loop

---
+++ bin/practice_code/loop/for_loop_example.dart
@@ -0,0 +1,10 @@
+// dart for loop
+/*
+initialization ; condition; increment/ decrement
+ */
+void main() {
+  for(int number = 10; number <= 50; number =number+1){
+    print('Result Number = $number Tuhin');
+  }
+  print('End Result sheet');
+}
