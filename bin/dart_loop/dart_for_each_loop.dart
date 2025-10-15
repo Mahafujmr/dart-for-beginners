4336278e1e65d3901f51f896c1241e314f6b89c8
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-15 23:24:32 +0600

    dart for eeach loop list and map example

---
+++ bin/dart_loop/dart_for_each_loop.dart
@@ -0,0 +1,23 @@
+// dart for each loop
+/*
+==> syntax ==>
+for(data type name ; new variable name; in; List variable name
+ */
+void main() {
+  // creation list
+  print('List of For Each Loop');
+  List<String> studentName=['Tuhin','Toma','Sormila'];
+  for(String name in studentName){
+    print('Student Name List = $name');
+  }
+  // creation map
+  print('Map of For Each Loop');
+  Map<int, String> userList = {
+    1:'Tuhin',
+    2:'Mahafuj',
+    3:'MR Tuhin',
+  };
+  for(String usrName in userList.values){
+    print('Name = $usrName');
+  }
+}
