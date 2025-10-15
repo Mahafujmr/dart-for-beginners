c6e0de3e2381eb7dd1915eb9875c324e95a80425
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-15 21:48:53 +0600

    dart for loop use list

---
+++ bin/dart_loop/loop_break_continue.dart
@@ -0,0 +1,17 @@
+// dart loop continue
+/*
+continue = শুধু একটি iteration skip করে, লুপ চলতে থাকে
+break = পুরো লুপ বন্ধ করে দেয়
+ */
+void main() {
+  for(int a =0; a<=50; a++){
+    // print(a); // before check condition result 10 then stop loop
+    if(a==5){
+      continue; // skip 5 then print
+    }
+    if(a==10){
+      break;
+    }
+    print(a); // after check condition result 9 then stop loop
+  }
+}
---
+++ bin/practice_code/loop/list_of_loop/list_of_loop.dart
@@ -0,0 +1,9 @@
+// dart list of loop
+void main() {
+  // creation list
+  // follow index number
+  List<String> cricketerName= ['Tuhin','Tamim','Rakib','Taskin','Shakib'];
+  for(int a=0; a< cricketerName.length; a++){
+    print('Cricketr Name List = $a => ${cricketerName[a]}');
+  }
+}
