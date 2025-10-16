47258e69573ff440b10fdf0d30379dc1307a4e94
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-16 23:59:06 +0600

    dart map code

---
+++ bin/practice_code/map/dart_map.dart
@@ -0,0 +1,12 @@
+// dart nested map
+void main() {
+  Map<int, Map<String, String >> student ={
+    1:{
+      'Name':'Tuhin'
+    },
+    2:{
+      'Name':'Mahafuj'
+    }
+  };
+  print(student);
+}
