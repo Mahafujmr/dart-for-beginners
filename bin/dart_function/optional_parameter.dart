0007bfe9d107a7a06a4a4a13c23aa30abaf3db81
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-23 22:53:24 +0600

    dart function practice

--- bin/dart_function/optional_parameter.dart
+++ bin/dart_function/optional_parameter.dart
@@ -1,11 +1,17 @@
 // dart code -optional parameter
 
 void main() {
+  // call function
   idCardInformation('Tuhin','Kusthia',22);
   idCardInformation('Tuhin',);
 }
 // creation function = > and parameter
 //String name| String address| int age ==> Parameter
+// [] -> optional
+/*
+ function call and then set parameter then optional parameter
+ data type before set null -> ?
+ */
 
 void idCardInformation (String name, [String ? address, int ? age]) {
 
---
+++ bin/practice_code/functtion/sample_function.dart
@@ -0,0 +1,13 @@
+// dart creation sample function
+void main() {
+  // call function
+  userInformation();
+  userInformation();
+}
+// always time new function create main function outside
+// creation function
+userInformation () {
+  print('This is mobile');
+  print('This is computer');
+  print('This is software');
+}
