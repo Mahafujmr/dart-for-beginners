39292e3185a1d1a568cc9445d36f28020a740b65
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-24 12:16:27 +0600

    dart string properties

---
+++ bin/string_interpolation/string_propertes.dart
@@ -0,0 +1,12 @@
+// dart string properties
+/*
+===> String properties ==>
+length - string text length
+toUpperCase () - all text capital letter show
+toLowerCase() - all text small letter show
+ */
+ void main() {
+  String name = 'Tuhin';
+  print(name);
+  print('String length = ${name.length}');
+}
