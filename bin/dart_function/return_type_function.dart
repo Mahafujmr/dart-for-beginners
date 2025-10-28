7a63191b58a0886e31136d43d0e2ab42cd342d71
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-10-28 22:17:13 +0600

    dart return type function

---
+++ bin/dart_function/return_type_function.dart
@@ -0,0 +1,11 @@
+// dart return type function
+void main() {
+  // function call
+  print(add(33, 22));
+}
+// function declare
+ add (int number , double numberOne){
+  double totalResult = number +numberOne;
+  // input return
+  return totalResult ; // 33+22=55.0
+}
