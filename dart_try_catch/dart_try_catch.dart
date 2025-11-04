4ff9581f5da35af3a52f8afc366380405909a153
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-11-04 21:15:34 +0600

    dart user input

--- dart_try_catch/dart_try_catch.dart
+++ dart_try_catch/dart_try_catch.dart
@@ -1,1 +1,9 @@
 // dart try catch
+import 'dart:io';
+
+void main() {
+ print('Your Age is :');
+ String ? youAge = stdin.readLineSync();
+ int age = int.parse(youAge!);
+ print('My Age is = $age');
+}
