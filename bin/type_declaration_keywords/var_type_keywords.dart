c8c08ab708da66ed641bab640c60b50cdb4fdcbd
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-11-16 21:11:44 +0600

    dart var type keywords example

---
+++ bin/type_declaration_keywords/var_type_keywords.dart
@@ -0,0 +1,14 @@
+// dart var type keywords
+// use var keywords then no use data type name
+// first time assign the value , then next time not change
+
+void main() {
+  var rollNumber = 100; // dart decide this value int type
+  var result = 3.22;   // dart decide this value double type
+  var studentName = 'MD Tuhin'; // this value string type
+  var isGroup = true; // this value boolean type
+
+  print('I am Md $studentName , MY Roll Number is $rollNumber, My Result \n'
+      'is $result and I am a student is =  $isGroup ');
+
+}
