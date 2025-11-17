7f4b83e7f9fe0d2d685a2fc23252ad9ba1cec813
MD. Mahafujur Rahman Tuhin <mrtuhin444@gmail.com>
2025-11-17 23:10:18 +0600

    dart var concept

---
+++ bin/dart_keyword/var_keyword.dart
@@ -0,0 +1,14 @@
+// dart var keyword
+// var use - support any data type like - int, double, string,bool
+void main() {
+  // var type variable creation
+  var rollNumber = 10; // var means this is int
+  var result = 3.33; // this is double
+
+  var studentName = 'Tuhin'; // this is String
+  var isStudent = true; // this is boolean
+
+  print('I am a $studentName, My Roll Number is $rollNumber \n'
+      'My Result is $result, and I am a student $isStudent');
+
+}
